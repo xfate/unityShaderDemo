cd6378faac73871797c81a0b49acc1d83b87383b
xfate <xfatenet@gmail.com>
2018-08-09 21:01:57 +0800

    blur background

---
+++ Assets/dof/DOF_CommandBuffer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+public class DOF_CommandBuffer : MonoBehaviour {
+    public Renderer targetRenderer;
+    private CommandBuffer m_CommandBuffer;
+    public Material m_mat;
+    public Camera m_cam;
+    private void OnEnable()
+    {
+        m_CommandBuffer = new CommandBuffer();
+        m_CommandBuffer.DrawRenderer(targetRenderer, m_mat,0,0);
+        // 放在后处理完之后再渲染
+        m_cam.AddCommandBuffer(CameraEvent.AfterImageEffects, m_CommandBuffer);
+        targetRenderer.enabled = false;
+    }
+    private void OnDisable()
+    {
+        //移除command buffer
+        m_cam.RemoveCommandBuffer(CameraEvent.AfterImageEffects, m_CommandBuffer);
+        m_CommandBuffer.Clear();
+        targetRenderer.enabled = true;
+    }
+}
