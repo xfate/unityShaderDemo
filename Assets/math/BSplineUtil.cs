fd470ead4e2427a48d22a4ce663698530e07b037
xfate <xfatenet@gmail.com>
2018-08-26 23:44:12 +0800

    B样条

---
+++ Assets/math/BSplineUtil.cs
@@ -0,0 +1,91 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BSplineUtil
+{
+
+    static Vector3 DeBoor(int j, int k,int i, float t,List<Vector3> controlPoints,List<float> knots)
+    {
+        if (j == 0)
+            return controlPoints[i];
+        else
+        {
+            float param = (t - knots[i]) / (knots[i + k- j] - knots[i]);
+            return ((1-param) * DeBoor(j - 1, k, i - 1, t, controlPoints, knots)
+                + param * DeBoor(j - 1, k, i, t, controlPoints, knots)); 
+        }
+    }
+
+    static List<Vector3> GetBspline(List<Vector3> controlPoints,float detail,string type)
+    {
+        List<Vector3> points = new List<Vector3>();
+        int degree = controlPoints.Count <= 3 ? controlPoints.Count - 1 : 3;
+        var knots = createKnots(controlPoints.Count,degree);
+
+        float zJump = (knots[knots.Count - 1 - degree] - knots[degree]) / detail;
+        float z;
+        Vector3 point = new Vector3();
+        for (int i = 0; i < detail; i++)
+        {
+            if (i == detail - 1 && type == "clamped")
+            {
+                point = controlPoints[controlPoints.Count - 1];
+            }
+            else
+            {
+                z = knots[degree] + i * zJump;
+                int zInt = whichInterval(z, knots);
+                point = DeBoor(degree, degree, zInt, z, controlPoints, knots);
+            }
+            points.Add(point);
+        }
+        return points;
+    }
+    static int whichInterval(float x,List<float> knots)
+    {
+        for (int i = 1; i < knots.Count - 1; i++)
+        {
+            if (x < knots[i])
+                return (i - 1);
+            else if (x == knots[knots.Count - 1])
+                return (knots.Count - 1);
+        }
+        return -1;
+    }
+    static List<float> createOpenKnots(int nControl, int degree)
+    {
+        int nKnots = nControl + degree + 1;
+
+        List<float> knots = new List<float>(nKnots);
+
+        for (int i = 0; i < nKnots; i++)
+        {
+            if (i < 1) knots[i] = 0;
+            else knots[i] = knots[i - 1] + 1;
+        }
+        return knots;
+    }
+    static List<float> createKnots(int nControl, int degree)
+    {
+        int nKnots = nControl + degree + 1;
+
+        List<float> knots = new List<float>(nKnots);
+        for (int i = 0; i < nKnots; i++)
+        {
+            if (i < degree + 1) //节点t 属于k-1，n+1
+            {
+                knots[i] = 0;
+            }
+            else if (i < nKnots - degree)
+            {
+                knots[i] = knots[i - 1] + 1;//k-1，n+1 在这个范围内递增
+            }
+            else
+            {
+                knots[i] = knots[i - 1];
+            }
+        }
+        return knots;
+    }
+}
---
+++ Assets/math/Bsp.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class Bsp : MonoBehaviour {
+
+    int count = 3;
+
+    public Image panel;
+    public Button btnGo;
+    public Text tipText;
+    bool isVaild = true;
+    public int num = 20;
+    List<Button> goList = new List<Button>();
+	void Start () {
+    }
+	
+    public void OnClick()
+    {
+        var go = GameObject.Instantiate(btnGo, panel.transform);
+        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        go.transform.position = point;
+        goList.Add(go);
+        isVaild = true;
+
+    }
+	// Update is called once per frame
+	void Update () {
+        if (isVaild)
+        {
+            isVaild = false;
+            RefreshText();
+  
+        }
+    }
+    void RefreshText()
+    {
+        
+        if (goList.Count <= 0)
+        {
+            tipText.text = "选择控制点";
+        }
+        else if (goList.Count == 1)
+        {
+            tipText.text = "选择控制点";
+        }
+    }
+    void DrawLine()
+    {
+
+    }
+    void OnGUI()
+    {
+        if(Event.current.keyCode == KeyCode.Space)
+        {
+            Event.current.mousePosition
+        }
+    }
+    void ButtonClick()
+    {
+        foreach (var go in goList)
+            GameObject.DestroyImmediate(go);
+        goList.Clear();
+        
+    }
+
+}
