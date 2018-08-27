af3dca9676e2f0f79eb8b9eda8930cacdec5f589
xfate <xfatenet@gmail.com>
2018-08-28 00:07:45 +0800

    bsp

--- Assets/math/BSplineUtil.cs
+++ Assets/math/BSplineUtil.cs
@@ -4,31 +4,41 @@ using UnityEngine;
 
 public class BSplineUtil
 {
-
-    public static Vector3 DeBoor(int j, int k,int i, float t,List<Vector3> controlPoints,List<float> knots)
+    public enum eBSPLINE_TYPE
+    {
+        eClamped = 0,
+        eOpened = 1,
+    }
+     
+    public static Vector3 DeBoor(int j, int k,int i, float t,List<Vector3> controlPoints,float[] knots)
     {
         if (j == 0)
             return controlPoints[i];
         else
         {
-            float param = (t - knots[i]) / (knots[i + k- j] - knots[i]);
+
+            float param = (t - knots[i]) / (knots[i + k+1- j] - knots[i]);
             return ((1-param) * DeBoor(j - 1, k, i - 1, t, controlPoints, knots)
                 + param * DeBoor(j - 1, k, i, t, controlPoints, knots)); 
         }
     }
 
-    public static List<Vector3> GetBspline(List<Vector3> controlPoints,float detail,string type)
+    public static List<Vector3> GetBspline(List<Vector3> controlPoints,int detail, eBSPLINE_TYPE type,int degree = -1)
     {
         List<Vector3> points = new List<Vector3>();
-        int degree = controlPoints.Count <= 3 ? controlPoints.Count - 1 : 3;
-        var knots = createKnots(controlPoints.Count,degree);
-
-        float zJump = (knots[knots.Count - 1 - degree] - knots[degree]) / detail;
+        if (degree == -1 || degree < 3)
+            degree = controlPoints.Count <= 3 ? controlPoints.Count - 1 : 3;
+        float[] knots = null;
+        if (type == eBSPLINE_TYPE.eClamped)
+            knots = createKnots(controlPoints.Count,degree);
+        else
+            knots = createOpenKnots(controlPoints.Count, degree);
+        float zJump = (knots[knots.Length - 1 - degree] - knots[degree]) / (detail-1);
         float z;
         Vector3 point = new Vector3();
         for (int i = 0; i < detail; i++)
         {
-            if (i == detail - 1 && type == "clamped")
+            if (i == detail - 1 && type == eBSPLINE_TYPE.eClamped)
             {
                 point = controlPoints[controlPoints.Count - 1];
             }
@@ -42,22 +52,22 @@ public class BSplineUtil
         }
         return points;
     }
-    public static int whichInterval(float x,List<float> knots)
+    public static int whichInterval(float x,float[] knots)
     {
-        for (int i = 1; i < knots.Count - 1; i++)
+        for (int i = 1; i < knots.Length - 1; i++)
         {
             if (x < knots[i])
                 return (i - 1);
-            else if (x == knots[knots.Count - 1])
-                return (knots.Count - 1);
+            else if (x == knots[knots.Length - 1])
+                return (knots.Length - 1);
         }
         return -1;
     }
-    public static List<float> createOpenKnots(int nControl, int degree)
+    public static float[] createOpenKnots(int nControl, int degree)
     {
         int nKnots = nControl + degree + 1;
 
-        List<float> knots = new List<float>(nKnots);
+        var knots = new float[nKnots];
 
         for (int i = 0; i < nKnots; i++)
         {
@@ -66,11 +76,11 @@ public class BSplineUtil
         }
         return knots;
     }
-    public static List<float> createKnots(int nControl, int degree)
+    public static float[] createKnots(int nControl, int degree)
     {
         int nKnots = nControl + degree + 1;
 
-        List<float> knots = new List<float>(nKnots);
+        var knots = new float[nKnots];
         for (int i = 0; i < nKnots; i++)
         {
             if (i < degree + 1) //节点t 属于k-1，n+1
--- Assets/math/Bsp.cs
+++ Assets/math/Bsp.cs
@@ -2,66 +2,125 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
-public class Bsp : MonoBehaviour {
+public class Bsp : MonoBehaviour
+{
 
     int count = 3;
-
+    public Material LineMat;
     public Image panel;
     public Button btnGo;
     public Text tipText;
     bool isVaild = true;
-    public int num = 20;
+    public int num = 40;
+    public Canvas canvas;
+    List<Vector3> posList = new List<Vector3>();
     List<Button> goList = new List<Button>();
-	void Start () {
+    bool isRenderLine = false;
+    public BSplineUtil.eBSPLINE_TYPE type = BSplineUtil.eBSPLINE_TYPE.eClamped;
+    [Range(3,100)]
+    public int degree = 3;
+    void Start()
+    {
+
     }
-	
+
     public void OnClick()
     {
         var go = GameObject.Instantiate(btnGo, panel.transform);
-        Vector3 point = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-        go.transform.position = point;
+        go.gameObject.SetActive(true);
+        Vector2 _pos = Vector2.one;
+        RectTransformUtility.ScreenPointToLocalPointInRectangle(canvas.transform as RectTransform,
+                    Input.mousePosition, canvas.worldCamera, out _pos);
+
+        go.transform.localPosition = new Vector3(_pos.x, _pos.y, 0);
         goList.Add(go);
+        posList.Add(go.transform.position);
         isVaild = true;
 
+
     }
-	// Update is called once per frame
-	void Update () {
+    // Update is called once per frame
+    void Update()
+    {
         if (isVaild)
         {
             isVaild = false;
             RefreshText();
-  
+
+        }
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            Clear();
+            isVaild = true;
+            isRenderLine = false;
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            isRenderLine = true;
+
         }
     }
     void RefreshText()
     {
-        
+
         if (goList.Count <= 0)
         {
-            tipText.text = "选择控制点";
+            tipText.text = "先选择起始点";
         }
         else if (goList.Count == 1)
         {
             tipText.text = "选择控制点";
         }
+
+
+    }
+    private void OnRenderObject()
+    {
+        DrawLine();
     }
     void DrawLine()
     {
-
-    }
-    void OnGUI()
-    {
-        if(Event.current.keyCode == KeyCode.Space)
+        if (!isRenderLine || LineMat == null)
+            return;
+        for (int i = 0; i < posList.Count-1; ++i)
         {
-            
+            var points = new Vector3[] { posList[i], posList[i+1] };
+            DrawLine_Impl(points, Color.red);
         }
+        var  bspPoints = BSplineUtil.GetBspline(posList, num, type);
+        DrawLine_Impl(bspPoints.ToArray(), Color.blue);
     }
-    void ButtonClick()
+
+    void Clear()
     {
         foreach (var go in goList)
-            GameObject.DestroyImmediate(go);
+        {
+            GameObject.DestroyImmediate(go.gameObject);
+
+        }
+        posList.Clear();
         goList.Clear();
-        
+
     }
+    void DrawLine_Impl(Vector3[] verts, Color color)
+    {
+        GL.PushMatrix(); //保存当前Matirx  
+        LineMat.SetPass(0); //刷新当前材质  
+        GL.LoadPixelMatrix();//设置pixelMatrix  
+        GL.Color(color);
+        GL.Begin(GL.LINES);
+
+        for (int i = 0; i < verts.Length; ++i)
+        {
+            if (i - 1 >= 0)
+                GL.Vertex(verts[i - 1]);
+            else
+                GL.Vertex(verts[0]);
+            GL.Vertex(verts[i]);
+        }
+        GL.End();
+        GL.PopMatrix();//读取之前的Matrix 
+    }
+
 
 }
